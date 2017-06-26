f2a51173cb9b9ff9dd7cabb859c7380c3c5eedbf
Fjodor Zotaj <fjodorzotaj@gmail.com>
2017-06-25 18:43:14 -0700

    Added the API key for the first json call

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DataProvider } from '../../providers/data/data';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  games = [];
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _data: DataProvider) {
+  
+    this._data.getGames('6', 0)
+      .subscribe(res => this.games = res);
+
+      //console.log(this.games);
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
   }
 
 }
